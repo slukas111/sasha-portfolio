7419af82ed34a491b3b92ca1e93a53e76a7fb58c
Sasha Lukas <sashamati@gmail.com>
2020-10-16 15:13:08 -0700

    added image for hero card. added some text for descriptive about me

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import profimage from "../assets/hero-img.png";
+
 
 export class Hero extends Component {
     render() {
         return (
-            <div>
-                I'm the developer --- Hero component
-            </div>
+
+            <div className="container">
+                <div className="row">
+                <div className="hero-card col-md-6 col-md-push-6">
+                    <img src={profimage} alt="BG-Card" className="hero-img" />
+                </div>
+
+                <div className="col-md-6 col-md-pull-6">
+            <h1 className="hero-tag">
+              Hi, I am <br /> 
+              <strong>Sasha Lukas.</strong>
+            </h1>
+            <p className="about-tag">
+            Software Engineer with an interest in backend. Recent graduate from Kenzie Academy Fullstack program. 
+            I am still pursuing a computer science degree through Olympic College, Bremerton WA. Some knack-experience with designs, photography, and film. 
+            </p>
+                </div>
+              </div>
+              </div>
+
+
         )
     }
 }
