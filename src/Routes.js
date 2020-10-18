b270384e36d6c9d062a3d2c4ecc24458ddae496a
Sasha Lukas <sashamati@gmail.com>
2020-10-18 15:24:55 -0700

    added the project components through routes and display texts

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import App from "./App";
+import Backend from "./components/Projects/Backend";
+import Frontend from "./components/Projects/Frontend";
+import Sketch from "./components/Projects/Sketch";
 
 
 
@@ -16,6 +19,10 @@ class Routes extends Component {
       <Router>
         <Fragment>
           <Route exact path="/" component={App} />
+
+          <Route exact path="/frontend" component={Frontend} />
+          <Route exact path="/backend" component={Backend} />
+          <Route exact path="/sketch" component={Sketch} />
         </Fragment>
       </Router>
     );
